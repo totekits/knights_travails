09c41efc3e29432bda979be6ac4f18c8856c8ade
totekits <tote.kits@gmail.com>
2023-11-23 09:41:21 +0700

    Write pseudocode

---
+++ main.rb
@@ -0,0 +1,4 @@
+def knight_moves(start, finish) # take arr start, arr finish, return arr shortest path and depth
+  # generate all possible paths from start to finish (use recursion), return a tree
+  # find the shortest path (use BFS), return arr path
+  # find its depth, return int depth
