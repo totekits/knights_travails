4e2519c85fb6c96422b82e93543f6ab0b2ff8893
totekits <tote.kits@gmail.com>
2023-11-24 09:26:22 +0700

    Def Knight class, Move find_night_moves to knight.rb and change its name to find_possible_moves

---
+++ lib/knight.rb
@@ -0,0 +1,21 @@
+class knight
+  attr_accessor :position, possible_moves
+
+  def initialize(position)
+    @position = position
+    @possible_moves = find_possible_moves(position)
+  end
+
+  def find_possible_moves(position)
+    possible_moves = []
+    mods = [[0,-1],[0,1],[2,1],[2,-1],[1,-2],[-1,-2],[-2,-1],[-2,1]]
+
+    mods.each do |mod|
+      x = position[0] + mod[0]
+      y = position[1] + mod[1]
+      possible_moves << [x,y] if x.between(0, 7) && y.between(0, 7)
+    end
+
+    possible_moves
+  end
+end
--- main.rb
+++ main.rb
@@ -1,14 +1,4 @@
-def find_knight_moves(position, moves=[])
-  mods = [[0,-1],[0,1],[2,1],[2,-1],[1,-2],[-1,-2],[-2,-1],[-2,1]]
-
-  mods.each do |mod|
-    x = position[0] + mod[0]
-    y = position[1] + mod[1]
-    moves << [x,y] if x.between(0, 7) && y.between(0, 7)
-  end
-
-  moves
-end
+require_relative 'lib/knight'
 
 def knight_moves(start, finish) # take arr start, arr finish, return arr shortest path and depth
   # generate all possible paths from start to finish (use recursion), return a tree
