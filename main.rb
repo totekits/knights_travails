a645454c2648bf927ae806fea733206836fe86e1
totekits <tote.kits@gmail.com>
2023-11-24 13:51:08 +0700

    Def find_shortest_path method

--- main.rb
+++ main.rb
@@ -14,6 +14,25 @@ def build_tree(start, target, visited = Set.new, node = nil)
   node
 end
 
+def find_shortest_path(node, path = [])
+  return path + [node.position] if node.pointers.empty?
+
+  shortest_path = nil
+
+  node.pointers.each do |point|
+    ref_path = find_shortest_path(point, path + [node.position])
+    shortest_path = ref_path if shortest_path.nil? || ref_path.size < shortest_path.size
+  end
+
+  shortest_path
+end
+
+def knight_moves(start, target, path = [], edges = 0)
+  return if start == target
+
+  node = build_tree(start, target)
+  
+  path = node.find_shortest_path(node)
 
 # def knight_moves(start, finish) # take arr start, arr finish, return arr shortest path and depth
 #   # generate all possible paths from start to finish (use recursion), return a tree
